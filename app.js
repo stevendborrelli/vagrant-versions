487b8e4d848830852986aec4d9fed35e285098e9
Steven Borrelli <steve@borrelli.org>
2013-09-30 10:04:29 -0500

    Now reqs web page for each version tag
    Script will find all the tags on the main page, and
    then fetch the page for each of the tags.

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
+var optimist = require("optimist");
 var request = require("request");
 var cheerio = require("cheerio");
 
 
 var url = 'http://downloads.vagrantup.com' 
 
+var tags = []; 
 
 function get_tags(url) {
         request(url, function(err, resp, body) {
+           var t = []; 
            if (err)
              throw err; 
            $ = cheerio.load(body);
-           $('a.tag' ).each(function(tag) {
-              console.log($(this).text());
-           }); 
-        }); 
-
+           //console.log(body);
+           $('a.tag').each(function(tag) {
+              //console.log(tag);
+              get_package_info(url + '/tags/', $(this).text()); 
+              t.push($(this).text());
+              //console.log(tags);
+           });
+           console.log(t);
+        });
 };
 
-get_tags(url); 
+function get_package_info(url, tag){
+        u = url + tag;
+        console.log(u); 
+        request(url + tag, function(err, resp, body) {
+        $ = cheerio.load(body); 
+        console.log(body); 
+        }); 
+
+}; 
+
+
+get_tags(url);
+//console.log(tags); 
