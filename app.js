e955ba673e7ce4a6648e6371a7191947855c58be
Steven Borrelli <steve@borrelli.org>
2013-09-30 11:11:39 -0500

    cmdline parsing to get all tags or info a tag

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 var argv = require("optimist")
+   .boolean(['fetchtags'])
    .default('url', 'http://downloads.vagrantup.com')
    .default('file', 'checksums.json')
+   .default('tag', 'v1.3.3')
+   .default('tagpath', '/tags/') 
    .argv; 
 
 var request = require("request");
 var cheerio = require("cheerio");
 
-
-
-var tags = []; 
-
 function get_tags(url) {
         request(url, function(err, resp, body) {
-           var t = []; 
+           var t = [];
            if (err)
              throw err; 
            $ = cheerio.load(body);
            //console.log(body);
            $('a.tag').each(function(tag) {
               //console.log(tag);
-              get_package_info(url + '/tags/', $(this).text()); 
+              get_package_info(url + '/tags/', $(this).text());
               t.push($(this).text());
               //console.log(tags);
            });
@@ -29,14 +28,17 @@ function get_tags(url) {
 
 function get_package_info(url, tag){
         u = url + tag;
-        console.log(u); 
+        console.log(u);
         request(url + tag, function(err, resp, body) {
         $ = cheerio.load(body); 
         console.log(body); 
         }); 
 
-}; 
+};
 
-
-get_tags(argv.url);
-//console.log(tags); 
+if (argv.fetchtags) { 
+    get_tags(argv.url); 
+} else
+{
+    get_package_info(argv.url + argv.tagpath , argv.tag);
+}
